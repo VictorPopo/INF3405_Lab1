7130fb669a856c0dca6b13f8e8397dfe8b6e3bdd
Maxime_Spencer <maxime.spencer@polymtl.ca>
2025-01-27 19:24:38 -0500

    allo

--- client/src/client_package/Client.java
+++ client/src/client_package/Client.java
@@ -5,6 +5,9 @@ public class Client {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Heyy");
+		System.out.println("Heyy");
+		System.out.println("Heyy");
+		System.out.println("Heyy");
 	}
 
 }
